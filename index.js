4c8cc76b1ecb333bb3c83f60f4841f15a572300a
Bozhena Luskavets <88252005+bozhenaluskavets@users.noreply.github.com>
2022-07-12 13:16:07 +0300

    loops optimization

--- index.js
+++ index.js
@@ -1,102 +1,56 @@
-let len = 7;
+const len = 7;
 let symbol = '*';
 let result = '';
 let space = ' ';
 
 //квадрат
 
-for (let i = 0; i < len; i++) {
-    for (j = 0; j < len; j++) {
-        result = result + symbol;
-    }
-
-    console.log(result);
-    result = '';
-}
-
-console.log(space);
+console.log((symbol.repeat(len) + '\n').repeat(len));
 
 //стрілка вниз
 
-for (let i = 0; i < len; i++) {
-    for (j = 0; j < i; j++) {
-        result = result + space;
-    }
-    
-    for (let k = 0; k < len - i * 2; k++) { 
-        result += symbol;
-    }
-
-    console.log(result);
-    result = '';
+for (let stars = len; stars >= 0; stars = stars - 2) {
+    console.log(space + symbol.repeat(stars));
+    space += ' ';
 }
-
-console.log(space)
 
 //стрілка вгору
 
-for (let i = 1; i <= len; i++) {
-    for (j = 1; j <= len - i; j++) {
-        result = result + space;
-    }
-    
-    for (let k = 0; k < 2 * i - 1; k++) {
-        result += symbol;
-    }
-
-    console.log(result);
-    result = '';
+for (let stars = 1; stars <= len; stars += 2) {
+    const spaces = (len - stars) / 2;
+    console.log(' '.repeat(spaces), symbol.repeat(stars));
 }
-
-console.log(space);
 
 //стрілка вліво
 
-for (let i = 1; i < len; i++) {
-    for (let j = 0; j < len - i; j++) {
-        result += space;
-    }
-
-    for (let k = 0; k < i; k++) {
-        result += symbol;
-    }
-
-    console.log(result);
-    result = '';
+for (let stars = 1; stars <= len / 2 + 1; stars++) {
+    const spaces = (len / 2 - stars + 1);
+    console.log(' '.repeat(spaces), symbol.repeat(stars));
 }
 
-for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - i; j++) {
-        result += symbol;
-    }
-  
-    for (let k = 0; k < i; k++) {
-        result = space + result;
-    }
-  
-    console.log(result);
-    result = '';
+for (let stars = len / 2; stars >= 1; stars--) {
+    const spaces = (len / 2 - stars + 1);
+    console.log(' '.repeat(spaces), symbol.repeat(stars));
 }
-
-console.log(space);
 
 //стрілка вправо
 
-for (let i = 1; i < len; i++) {
-
-    for (let j = 0; j < i; j++) {
-        result += symbol;
-    }
-
-    console.log(result);
-    result = '';
+for (let stars = 1; stars <= len / 2 + 1; stars++) {
+    console.log(symbol.repeat(stars));
 }
 
-for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - i; j++) {
-      result += symbol;
-    }
-  
-    console.log(result);
-    result = '';
+for (let stars = len / 2; stars >= 1; stars--) {
+    console.log(symbol.repeat(stars));
+}
+
+//ромб
+
+for (let stars = 1; stars <= len; stars += 2) {
+    const spaces = (len - stars) / 2;
+    console.log(' '.repeat(spaces), symbol.repeat(stars));
+}
+
+for (let stars = len - 2; stars >= 0; stars = stars - 2) {
+    const spaces = (len - stars) / 2;
+    console.log(' '.repeat(spaces), symbol.repeat(stars));
 }
